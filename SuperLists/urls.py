d9a4b26d73880d07e4b407f20b47a3ea0e700fd9
IMI-TRISTAN <s.shillitoe@sheffield.ac.uk>
2023-07-31 13:30:57 +0100

    passed unit test for home page display

--- SuperLists/urls.py
+++ SuperLists/urls.py
@@ -6,17 +6,16 @@ from datetime import datetime
 from django.urls import path
 from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
-from app import forms, views
+from lists import forms, views
 
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('contact/', views.contact, name='contact'),
-    path('about/', views.about, name='about'),
+    path('admin/', admin.site.urls),
     path('login/',
          LoginView.as_view
          (
-             template_name='app/login.html',
+             template_name='lists/login.html',
              authentication_form=forms.BootstrapAuthenticationForm,
              extra_context=
              {
@@ -26,5 +25,4 @@ urlpatterns = [
          ),
          name='login'),
     path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
-    path('admin/', admin.site.urls),
 ]
