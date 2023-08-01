c2dc0ea450f55599101b87695b036c320eaf05df
IMI-TRISTAN <s.shillitoe@sheffield.ac.uk>
2023-08-01 23:59:15 +0100

    extending functional test

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,8 @@
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -12,6 +15,9 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
+
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
+        self.assertIn('To-Do', header_text)
         self.fail('Finish the test!')
 
 if __name__ == '__main__':
