788725062363a19239a9f6bc8b860eb3afa1db96
IMI-TRISTAN <s.shillitoe@sheffield.ac.uk>
2023-08-02 19:57:43 +0100

    Functional test now checks that we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -11,15 +11,35 @@ class NewVisitorTest(unittest.TestCase):
 
     def tearDown(self) -> None:
         self.browser.quit()
-
+        
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
 
         header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
         self.assertIn('To-Do', header_text)
+
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'),
+                         'Enter a to-do item')
+
+        inputbox.send_keys('Buy ferret food')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+
+        #Generator expression that generates values on-the-fly as you iterate through them
+        #Similiar to a list comprehension but uses () not []
+        self.assertTrue(
+            any(row.text == '1: Buy ferret food' for row in rows)
+            )
+
+
         self.fail('Finish the test!')
 
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
+
+
+unittest.main(warnings='ignore')
 
