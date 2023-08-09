d26c824715e085c0e25cdc1c2994917baa31ce6d
IMI-TRISTAN <s.shillitoe@sheffield.ac.uk>
2023-08-09 01:18:21 +0100

    added List model

--- lists/models.py
+++ lists/models.py
@@ -8,3 +8,6 @@ from django.db import models
 # Create your models here.
 class Item(models.Model):
     text = models.TextField(default="")
+
+class List(models.Model):
+    id = models.AutoField(primary_key=True)
--- lists/tests.py
+++ lists/tests.py
@@ -8,7 +8,7 @@ from django.urls import resolve
 from django.test import TestCase
 from lists.views import home_page
 from django.http import HttpRequest
-from lists.models import Item
+from lists.models import Item, List
 
 # TODO: Configure your database in settings.py and sync before running tests.
 
@@ -51,15 +51,23 @@ class ListViewTest(TestCase):
 
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'The second (ever) list item'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(),2)
@@ -67,4 +75,6 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual('The first (ever) list item', first_saved_item.text)
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual('The second (ever) list item', second_saved_item.text)
+        self.assertEqual(second_saved_item.list, list_)
