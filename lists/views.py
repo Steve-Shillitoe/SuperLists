52f3e344460fd940eee478481dd73aec22a255d2
IMI-TRISTAN <s.shillitoe@sheffield.ac.uk>
2023-08-11 02:17:53 +0100

    update views.view_list function and its test

--- lists/views.py
+++ lists/views.py
@@ -13,10 +13,10 @@ def home_page(request):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    items = Item.objects.all(list=list_)
+    items = Item.objects.filter(list=list_)
     return render(request, 'lists/list.html', {'items':items})
 
 def new_list(request):
     list_ = List.objects.create()
     Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect('/lists/the-only-list-in-the-world/')
+    return redirect( f'/lists/{list_.id}/')
