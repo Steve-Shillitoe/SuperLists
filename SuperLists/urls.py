268b9ca9e78083372a6a058f9362a40260df5f04
IMI-TRISTAN <s.shillitoe@sheffield.ac.uk>
2023-08-12 01:09:09 +0100

    new URL and view for adding to existing lists.

--- SuperLists/urls.py
+++ SuperLists/urls.py
@@ -12,7 +12,8 @@ from lists import forms, views
 urlpatterns = [
     path('', views.home_page, name='home'),
     path('admin/', admin.site.urls),
-    path('lists/<str:list_id>/', views.view_list, name='view_list'),
     path('lists/new', views.new_list, name='new_list'),
+    path('lists/<int:list_id>/', views.view_list, name='view_list'),
+    path('lists/<int:list_id>/add_item', views.add_item, name='add_item')
    
 ]
