fd03bb7e5e54e303f3e580ff617b9dd7a4854bc8
IMI-TRISTAN <s.shillitoe@sheffield.ac.uk>
2023-08-12 12:43:15 +0100

    WIP creating app specific urls.py

--- SuperLists/urls.py
+++ SuperLists/urls.py
@@ -2,18 +2,15 @@
 Definition of urls for SuperLists.
 """
 
-from datetime import datetime
-from django.urls import path
+from django.urls import path, include
 from django.contrib import admin
-from django.contrib.auth.views import LoginView, LogoutView
-from lists import forms, views
+from lists import views as list_views
+from lists import urls as list_urls
 
 
 urlpatterns = [
-    path('', views.home_page, name='home'),
+    path('', list_views.home_page, name='home'),
     path('admin/', admin.site.urls),
-    path('lists/new', views.new_list, name='new_list'),
-    path('lists/<int:list_id>/', views.view_list, name='view_list'),
-    path('lists/<int:list_id>/add_item', views.add_item, name='add_item')
+    path('lists/', include(list_urls)),
    
 ]
